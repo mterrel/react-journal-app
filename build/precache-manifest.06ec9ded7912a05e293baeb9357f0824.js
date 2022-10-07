ba9d5ef4a70e1b4a669686bd3345d213e3b07c86
Adil Jawad <45437363+Adilien@users.noreply.github.com>
2022-10-07 17:02:32 +0100

    Added files

---
+++ build/precache-manifest.06ec9ded7912a05e293baeb9357f0824.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f836c6b405581a3eec01d1f41118cf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49ecc8f1baf163a64dea",
+    "url": "/static/css/2.bd8c781c.chunk.css"
+  },
+  {
+    "revision": "804590b7af5ec41b9091",
+    "url": "/static/css/main.b5b3e6e4.chunk.css"
+  },
+  {
+    "revision": "49ecc8f1baf163a64dea",
+    "url": "/static/js/2.9fd412e8.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.9fd412e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "804590b7af5ec41b9091",
+    "url": "/static/js/main.9a97196e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.9a97196e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "b182dce97c525509bf22668a180e3e6f",
+    "url": "/static/media/myJournal.b182dce9.png"
+  },
+  {
+    "revision": "2323284ef85bfeaca074c668b72109c6",
+    "url": "/static/media/roboto-all-300-normal.2323284e.woff"
+  },
+  {
+    "revision": "9b9ec29522d1bf8924ccc2d917e1807b",
+    "url": "/static/media/roboto-cyrillic-300-normal.9b9ec295.woff2"
+  },
+  {
+    "revision": "d04413353a32aed8a0db452373452870",
+    "url": "/static/media/roboto-cyrillic-ext-300-normal.d0441335.woff2"
+  },
+  {
+    "revision": "dcdaee374d5bbeab0a5ed5c8cf39a6cd",
+    "url": "/static/media/roboto-greek-300-normal.dcdaee37.woff2"
+  },
+  {
+    "revision": "c2be5367fbf0e1066261fd78eb103f4a",
+    "url": "/static/media/roboto-greek-ext-300-normal.c2be5367.woff2"
+  },
+  {
+    "revision": "b9c29351c46f3e8c8631c4002457f48a",
+    "url": "/static/media/roboto-latin-300-normal.b9c29351.woff2"
+  },
+  {
+    "revision": "716871ec15f054ec158445180fe280e1",
+    "url": "/static/media/roboto-latin-ext-300-normal.716871ec.woff2"
+  },
+  {
+    "revision": "48c684d99330969e3ce90b9e9da2d698",
+    "url": "/static/media/roboto-vietnamese-300-normal.48c684d9.woff2"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,35 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
+import path from "path";
+import {fileURLToPath} from 'url';
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const CONNECTION_URL = process.env.REACT_APP_CONNECTION_URL;
+const PORT = process.env.REACT_APP_PORT || 3000;;
+
+mongoose
+	.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => app.listen(PORT, () => console.log(`Listening on port: ${PORT}`)))
+	.catch((err) => console.log(err.message));
